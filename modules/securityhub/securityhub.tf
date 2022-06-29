d3114feed7e80a7340cad8f4cb6637829fb9b237
Rene Gulager Larsen <rel@whiteaway.com>
2022-06-29 11:03:36 +0200

    remobe comments

--- modules/securityhub/securityhub.tf
+++ modules/securityhub/securityhub.tf
@@ -4,11 +4,11 @@ resource "aws_securityhub_standards_subscription" "cis_aws_foundations_benchmark
   standards_arn = "arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/1.2.0"
 }
 
-/* enable on all new accounts
+/* enable on all new accounts*/
 resource "aws_securityhub_organization_configuration" "securityhub_configuration" {
   auto_enable = true
 }
-*/
+
 
 /**members*/
 /*
@@ -32,6 +32,14 @@ resource "aws_securityhub_member" "datalake-test" {
 resource "aws_securityhub_member" "main" {
   depends_on = [aws_securityhub_account.securityhub]
   account_id = "533026789209"
+  email      = "rel@whiteaway.com"
+  invite     = true
+}
+
+
+resource "aws_securityhub_member" "legasy-prod" {
+  depends_on = [aws_securityhub_account.securityhub]
+  account_id = "861318969373"
   email      = "rel@whiteaway.com"
   invite     = true
 }
